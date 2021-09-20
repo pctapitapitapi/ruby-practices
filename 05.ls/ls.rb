c07ecfe1d7f484110e697cada313a5a89fc888d0
pctapitapitapi <emi.yamamoto73@gmail.com>
2021-09-20 12:30:20 +0900

    メソッド名の変更とメソッドの再構成

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -9,74 +9,36 @@ def main
   opt.on('-a') { |v| params[:a] = v }
   opt.on('-r') { |v| params[:r] = v }
   opt.on('-l') { |v| params[:l] = v }
-
   opt.parse!(ARGV)
 
-  all_files = Dir.glob('*').sort
+  all_files = params[:a] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
+
   if params.empty?
-    build_list(all_files)
+    show_list(all_files)
     return
   end
-  all_files_a = option_a(all_files, params)
-  all_files_r = option_r(all_files_a, params)
-  all_files_l = option_l(all_files_r, params)
-  build_list(all_files_l) unless params[:l]
-end
 
-def option_a(all_files, params)
-  if params[:a]
-    Dir.glob('*', File::FNM_DOTMATCH).sort
-  else
-    all_files
-  end
-end
+  all_files = reverse_all_files(all_files) if params[:r]
 
-def option_r(all_files_a, params)
-  if params[:r]
-    all_files_a.reverse
-  else
-    all_files_a
-  end
-end
-
-def option_l(all_files_r, params)
   if params[:l]
-    built_option_l(all_files_r)
+    show_detailed_list(all_files)
   else
-    all_files_r
+    show_list(all_files)
   end
 end
 
-def built_option_l(all_files)
-  total = 0
-  all_files.each do |file|
-    file = File::Stat.new(file)
-    total += file.blocks
-  end
-  puts "total #{total}"
-  add_file_type(all_files)
+def reverse_all_files(all_files)
+  all_files.reverse
 end
 
-def build_list(all_files)
-  row_size = if (all_files.length % 3).zero?
-               all_files.length / 3
-             else
-               all_files.length / 3 + 1
-             end
+def show_list(all_files)
+  row_size = built_row_size(all_files)
   sliced_files = all_files.each_slice(row_size).to_a
   (row_size - sliced_files.last.length).times { sliced_files.last.push('') } if sliced_files.last.length < row_size
 
-  max_length_array = []
-  sliced_files.each do |files_element|
-    files_element.each do |file_element|
-      max_length_array << file_element.length
-    end
-  end
+  max_length_array = built_max_length_array(sliced_files)
   max_length = max_length_array.max
-  show_list(sliced_files, max_length)
-end
 
-def show_list(sliced_files, max_length)
   sliced_files.transpose.each do |files_element|
     file_names = []
     files_element.each do |file_element|
@@ -89,20 +51,58 @@ def show_list(sliced_files, max_length)
   end
 end
 
-def add_file_type(all_files)
-  all_files.each do |file|
-    case File.ftype(file)
-    when 'file'
-      print '-'
-    when 'directory'
-      print 'd'
+def built_row_size(all_files)
+  if (all_files.length % 3).zero?
+    all_files.length / 3
+  else
+    all_files.length / 3 + 1
+  end
+end
+
+def built_max_length_array(sliced_files)
+  max_length_array = []
+  sliced_files.each do |files_element|
+    files_element.each do |file_element|
+      max_length_array << file_element.length
     end
-    add_permission(file)
-    add_hard_link(file)
-    add_user_detail(file)
-    add_byte_size(file)
-    add_file_time(file)
-    add_file_name(file)
+  end
+  max_length_array
+end
+
+def show_detailed_list(all_files)
+  total = built_total(all_files)
+  puts "total #{total}"
+  detailed_list = {}
+  all_files.each do |file|
+    detailed_list[:file_type] = add_file_type(file)
+    detailed_list[:permission] = add_permission(file)
+    detailed_list[:hard_link] = add_hard_link(file)
+    detailed_list[:user_detail] = add_user_detail(file)
+    detailed_list[:byte_size] = add_byte_size(file)
+    detailed_list[:file_time] = add_file_time(file)
+    detailed_list[:file_name] = File.basename(file)
+    detailed_list.each do |_key, value|
+      print value
+    end
+    puts
+  end
+end
+
+def built_total(all_files)
+  total = 0
+  all_files.each do |file|
+    file = File::Stat.new(file)
+    total += file.blocks
+  end
+  total
+end
+
+def add_file_type(file)
+  case File.ftype(file)
+  when 'file'
+    '-'
+  when 'directory'
+    'd'
   end
 end
 
@@ -124,13 +124,13 @@ def add_permission(file)
   group_permission = permission_numbers[-2]
   other_permission = permission_numbers[-1]
   permissions = permission_selections.values_at(user_permission, group_permission, other_permission)
-  print permissions.join
+  permissions.join
 end
 
 def add_hard_link(file)
   file = File::Stat.new(file)
   hard_link_number = file.nlink
-  print hard_link_number.to_s.rjust(6)
+  hard_link_number.to_s.rjust(6)
 end
 
 def add_user_detail(file)
@@ -139,22 +139,17 @@ def add_user_detail(file)
   user_name = Etc.getpwuid(user_id).name
   group_id = file.gid
   group_name = Etc.getgrgid(group_id).name
-  print " #{user_name}"
-  print "  #{group_name}"
+  " #{user_name}  #{group_name}"
 end
 
 def add_byte_size(file)
   file = File::Stat.new(file)
-  print "  #{file.size.to_s.rjust(6)}"
+  "  #{file.size.to_s.rjust(6)}"
 end
 
 def add_file_time(file)
   file = File::Stat.new(file)
-  print " #{file.mtime.strftime('%_m %d %H:%M')} "
-end
-
-def add_file_name(file)
-  puts File.basename(file)
+  " #{file.mtime.strftime('%_m %d %H:%M')} "
 end
 
 main
